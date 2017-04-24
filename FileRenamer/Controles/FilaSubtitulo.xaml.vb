08763a509f4cc60b86b4e42555dd3d89e5dc6769
Andres <andres.cirulo@gmail.com>
2017-04-24 12:30:55 -0300

    Agregado control de filas de la lista de subtitulos

---
+++ FileRenamer/Controles/FilaSubtitulo.xaml.vb
@@ -0,0 +1,23 @@
+﻿Public Class FilaSubtitulo
+
+    Public Video As Video
+
+    Public Sub New(ByRef video As Video)
+
+        ' Esta llamada es exigida por el diseñador.
+        InitializeComponent()
+
+        ' Agregue cualquier inicialización después de la llamada a InitializeComponent().
+        Me.video = video
+        chkElegido.Content = video.Nombre & "." & video.Extension
+        lblSubtitulo.Content = video.Subtitulo.Nombre & "." & video.Subtitulo.Extension
+    End Sub
+
+    Public Function IsSelected() As Boolean
+        Return chkElegido.IsChecked
+    End Function
+
+    Public Sub SetSelected(selected As Boolean)
+        chkElegido.IsChecked = selected
+    End Sub
+End Class
--- FileRenamer/Managers/SubtitulosManager.vb
+++ FileRenamer/Managers/SubtitulosManager.vb
@@ -1,6 +1,5 @@
 ﻿Imports System.IO
 Imports System.Text.RegularExpressions
-Imports FileRenamer
 
 Public Class SubtitulosManager
 
