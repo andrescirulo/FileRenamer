47831e76c1871790229f61c661da120983a45a09
Andres <andres.cirulo@gmail.com>
2017-05-14 21:29:17 -0300

    Avanzada version a 1.0.3

--- FileRenamer/My Project/AssemblyInfo.vb
+++ FileRenamer/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Windows
 ' mediante el carácter '*', como se muestra a continuación:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.2.0")>
-<Assembly: AssemblyFileVersion("1.0.2.0")>
+<Assembly: AssemblyVersion("1.0.3.0")>
+<Assembly: AssemblyFileVersion("1.0.3.0")>
